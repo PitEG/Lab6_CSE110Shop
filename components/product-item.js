0902e7b49c4ac50b8bb1abdef85433cde929cefe
Enrique Gan <PitEG@github.com>
2021-02-12 00:36:42 -0800

    components are being added

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,48 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor(list, link, img, pTitle, pPrice, button) {
+    super();
+
+    this.attachShadow({mode : 'open'});
+
+    this.list = document.createElement('li');
+    this.list.setAttribute('class', 'product');
+
+    this.link = document.createElement('link');
+    this.link.setAttribute('rel', 'stylesheet');
+    this.link.setAttribute('href', 'styles/styles.css');
+
+    this.img = document.createElement('img');
+    this.img.setAttribute('width', 200);
+
+    this.pTitle = document.createElement('p');
+    this.pTitle.setAttribute('class', 'title');
+
+    this.pPrice = document.createElement('p');
+    this.pPrice.setAttribute('class', 'price');
+
+    this.button = document.createElement('button');
+    this.button.textContent = 'Add to Cart'; 
+
+    this.list.append(this.img);
+    this.list.append(this.pTitle);
+    this.list.append(this.pPrice);
+    this.list.append(this.button);
+
+    this.shadowRoot.append(this.list);
+    this.shadowRoot.append(this.link);
+  }
+
+  fill(imgSrc, title, price) {
+    this.img.setAttribute('src', imgSrc);
+    this.img.setAttribute('alt', title);
+
+    this.textContent = "$" + price; 
+    this.pTitle.textContent = title;
+
+    //button.setAttribute('onclick', alert('Added to Cart!'))
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,20 @@
 // Script.js
 
+function populateWithProducts() {
+  let objects = JSON.parse(localStorage.getItem("products"));
+
+  for (let i = 0; i < objects.length; i++) {
+    let product = objects[i];
+    let productElement = document.createElement('product-item');
+    productElement.fill(product['image'], product['title'], product['price']);
+    document.getElementById('product-list').appendChild(productElement);
+  }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  fetch("https://fakestoreapi.com/products")
+    .then(response => response.json())
+    .then(data => localStorage.setItem("products", JSON.stringify(data)));
+  populateWithProducts();
 });
